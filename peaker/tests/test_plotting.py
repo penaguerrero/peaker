9ba1048b2c8b68789d78d8d345540d29a801f6dd
Maria Pena-Guerrero <pena@stsci.edu>
2026-04-13 10:55:39 -0400

    add local timezone for tests

--- peaker/tests/test_plotting.py
+++ peaker/tests/test_plotting.py
@@ -9,7 +9,7 @@ def test_create_pdf(tmpdir):
     data_dir = script_dir / "Data"
 
     # Create the dictionary of the tests
-    tests_ran, oldest_date, latest_date = parse_xmls(data_dir)
+    tests_ran, oldest_date, latest_date = parse_xmls(data_dir, "EST")
 
     # Make the plots
     plots = mk_plots(tests_ran, tmpdir)
