9bc942ae34d90a2b102f243b12b01bbd550ba084
Maria Pena-Guerrero <pena@stsci.edu>
2026-04-13 10:22:01 -0400

    ensure local time for expected dates in xml test

--- peaker/tests/test_parse_xmls.py
+++ peaker/tests/test_parse_xmls.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timezone
 
 from peaker.parse_xmls import parse_xmls
 
@@ -13,8 +13,8 @@ def test_parse_xmls():
 
     # Set the expected oldest and latest dates
     date_format = "%Y-%m-%d"
-    expected_oldest_date = datetime.strptime("2026-02-09", date_format)
-    expected_latest_date = datetime.strptime("2026-02-13", date_format)
+    expected_oldest_date = datetime.strptime("2026-02-09", date_format).astimezone(timezone.utc)
+    expected_latest_date = datetime.strptime("2026-02-13", date_format).astimezone(timezone.utc)
     # Make sure the dates are in local time
     expected_oldest_date = expected_oldest_date.astimezone().date()
     expected_latest_date = expected_latest_date.astimezone().date()
